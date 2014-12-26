27d7d2b9dd1b6a8b7e1cf11fc3b6ccc7250da947
Karthik Sirasanagandla <sirkarthik@gmail.com>
2014-12-26 22:26:34 +0530

    Changing after_create callback to after_commit for background jobs

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,10 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  after_create :create_twilio_subaccount
+  # Use after_commit instead of after_create
+  # Reference: http://rails-bestpractices.com/posts/695-use-after_commit
+  # References: http://www.codebeerstartups.com/2012/11/use-after_commit-instead-of-active-record-callbacks-to-avoid-unexpected-errors
+  after_commit :create_twilio_subaccount, on: :create
 
   def create_twilio_subaccount
     return if Rails.env.test?
